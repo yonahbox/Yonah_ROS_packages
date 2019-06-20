cccf4d25ac67d671d36f4e4660c88f2675c4a676
seahhorse <shaoxuan.seah@gmail.com>
2019-06-20 13:03:40 +0800

    Initial Add

---
+++ bonedata_ws/ground_ssh_connection.sh
@@ -0,0 +1,1 @@
+ssh -i "YonahAWSPrivateKey-8May19.pem" ubuntu@ec2-18-138-24-228.ap-southeast-1.compute.amazonaws.com -R 4000:localhost:4000
---
+++ bonedata_ws/launch_ros.sh
@@ -0,0 +1,2 @@
+roslaunch mavros apm_test.launch
+rosrun mavros gcs_bridge _gcs_url:='udp://@localhost:5001'
