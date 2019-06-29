75ea3d789d938c864dd679434165939ea1bdf5f7
Lau Yan Han <sps08.lauyanhan@gmail.com>
2019-06-29 16:37:16 +0800

    SMS_rx.py: Convert to OOP style
    This is to prepare for subscription to more MAVROS topics

--- bonesms_ws/src/yonah/src/SMS_tx.py
+++ bonesms_ws/src/yonah/src/SMS_tx.py
@@ -5,23 +5,25 @@ import os
 from mavros_msgs.msg import State
 import subprocess
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + " Nemo is mode: %s armed: %s", data.mode, data.armed)
-    try:
-        # Sending SMS to Ground Control
-        RUTLATLON = subprocess.check_output(["ssh", "root@192.168.1.1", "gpsctl -i && gpsctl -x"], shell=False)
-        # LON = subprocess.check_output(["ssh", "root@192.168.1.1", "gpsctl -x"], shell=False)
-        cmd2RUT = subprocess.call(["ssh", "root@192.168.1.1", "gsmctl -S -s '+6591993191 Nemo2 Lat:%s,Mode:%s,Arm:%s'"%(RUTLATLON, data.mode, data.armed)], shell=False)
-    except(subprocess.CalledProcessError):
-        print("SSH process into router has been killed.")
+class SMStx():
 
-def prepare():
-    rospy.init_node('SMS_tx', anonymous=False)
-    rospy.Subscriber("mavros/state", State, callback)
-    rospy.spin()
+    def callback(self, data):
+        rospy.loginfo(rospy.get_caller_id() + " Nemo is mode: %s armed: %s", data.mode, data.armed)
+        try:
+            # Sending SMS to Ground Control
+            RUTLATLON = subprocess.check_output(["ssh", "root@192.168.1.1", "gpsctl -i && gpsctl -x"], shell=False)
+            cmd2RUT = subprocess.call(["ssh", "root@192.168.1.1", "gsmctl -S -s '+6591993191 Nemo2 Lat:%s,Mode:%s,Arm:%s'"%(RUTLATLON, data.mode, data.armed)], shell=False)
+        except(subprocess.CalledProcessError):
+            print("SSH process into router has been killed.")
+
+    def prepare(self):
+        rospy.init_node('SMS_tx', anonymous=False)
+        rospy.Subscriber("mavros/state", State, self.callback)
+        rospy.spin()
 
 if __name__=='__main__':
-    prepare()
+    run = SMStx()
+    run.prepare()
 
 #Old Ways
 #    cmd2RUT = "echo Mode: %s Arm: %s | nc 192.168.1.1 5002"%(data.mode, data.armed)
