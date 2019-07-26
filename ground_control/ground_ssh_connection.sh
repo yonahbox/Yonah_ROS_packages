19e4dd05d8a7fbbad89e44349f4d9134a271eb67
seahhorse <shaoxuan.seah@gmail.com>
2019-07-26 13:17:00 +0800

    Stable version 26-Jul with file hierarchy

---
+++ ground_control/ground_ssh_connection.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh -i $(find -name *YonahAWSPrivateKey-2Jul19) ubuntu@ec2-18-138-24-228.ap-southeast-1.compute.amazonaws.com -L 4000:localhost:4000 -L 4001:localhost:4001 -R 4002:localhost:4002
