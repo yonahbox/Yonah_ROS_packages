345f9520a0457b12bbf43786e780ecf83b83bbfe
Lau Yan Han <sps08.lauyanhan@gmail.com>
2020-05-06 16:22:18 +0800

    sbd: Add test files for web server communication

---
+++ ogc_ws/src/sbd/test/mt_url_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+'''
+mt_url_test: Test script to send MT msg to Rockblock through RB server
+'''
+
+import binascii
+import requests
+
+url = 'https://core.rock7.com/rockblock/MT'
+values = {'imei':'', 'username':'', 'password':'', 'data':''}
+
+# Insert msg
+msg = 'Hello'
+msg = msg.encode()
+values['data'] = binascii.hexlify(msg).decode()
+
+# Insert IMEI and credentials
+fp = open('whitelist.txt', 'r') # replace whitelist with a better name
+values['imei'] = fp.readline().replace('\n','')
+values['username'] = fp.readline().replace('\n','')
+values['password'] = fp.readline().replace('\n','')
+
+#print(values)
+
+# Send HTTP post request to Rock 7 server
+response = requests.post(url, data=values)
+print(response.text)
