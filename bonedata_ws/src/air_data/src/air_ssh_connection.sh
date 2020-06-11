9a0bdf5dae2a533995ba06bb368aa7586e8e0d8e
Lau Yan Han <sps08.lauyanhan@gmail.com>
2020-06-11 10:51:11 +0800

    air_ssh_connection: Remove AWS Private Key reference
    We will copy our device public keys directly into the server

--- bonedata_ws/src/air_data/src/air_ssh_connection.sh
+++ bonedata_ws/src/air_data/src/air_ssh_connection.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i /home/ubuntu/Yonah_ROS_packages/bonedata_ws/src/air_data/src/YonahNemo5.pem ubuntu@ec2-18-138-24-228.ap-southeast-1.compute.amazonaws.com -L 4000:localhost:$1 -L 4001:localhost:$2 -L 4002:localhost:$3
+ssh ubuntu@ec2-18-138-24-228.ap-southeast-1.compute.amazonaws.com -L 4000:localhost:$1 -L 4001:localhost:$2 -L 4002:localhost:$3
