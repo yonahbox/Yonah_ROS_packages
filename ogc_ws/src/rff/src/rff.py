c1f03be4f871fd5d506ff0ed7de451935af13994
huachen24 <60135779+huachen24@users.noreply.github.com>
2020-07-17 15:07:04 +0800

    Update rff.py

--- ogc_ws/src/rff/src/rff.py
+++ ogc_ws/src/rff/src/rff.py
@@ -120,8 +120,7 @@ class Button:
 
 if __name__ == "__main__":
 	rospy.init_node('rff', anonymous=False, disable_signals=True)
-	home_dir = str(Path.home())
-	waypointsfolder = home_dir + "/Waypoints/"
+	waypointsfolder = rospy.get_param('~waypoint_folder', '/home/ubuntu/Waypoints/')
 	rff = RFF()
 
 	try:
