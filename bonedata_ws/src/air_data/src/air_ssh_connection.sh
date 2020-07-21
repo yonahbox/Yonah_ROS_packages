28df7e69bab5e6d222ba6d5e312a0a5789af1c87
Lau Yan Han <sps08.lauyanhan@gmail.com>
2020-07-21 15:37:22 +0800

    air_data: Break server ip into ros param

--- bonedata_ws/src/air_data/src/air_ssh_connection.sh
+++ bonedata_ws/src/air_data/src/air_ssh_connection.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh ubuntu@ec2-18-138-24-228.ap-southeast-1.compute.amazonaws.com -L 4000:localhost:$1 -L 4001:localhost:$2 -L 4002:localhost:$3
+ssh $1@$2 -L 4000:localhost:$3 -L 4001:localhost:$4 -L 4002:localhost:$5
