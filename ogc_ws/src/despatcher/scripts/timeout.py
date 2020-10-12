e0f2376e18b72c3853506be7330055eb6b278053
danikrisdayadi <kanisiusdani@gmail.com>
2020-10-12 23:15:31 +0800

    fixed shutdown issue

--- ogc_ws/src/despatcher/scripts/timeout.py
+++ ogc_ws/src/despatcher/scripts/timeout.py
@@ -16,7 +16,6 @@ class MessageTimer():
     def advance(self):
         self.reset_timer()
         self.client()
-        pass
 
     def reset_timer(self):
         self._watchdog = self.timeout
@@ -27,7 +26,10 @@ class MessageTimer():
             rospy.logwarn("TIMER HAS RUN OUT")
             rospy.logwarn(self.status_checker())
             self.status = -1
-            self.watcher.shutdown()
+            self.stop_timer()
+
+    def stop_timer(self):
+        self.watcher.shutdown()
             
     def status_checker(self):
         if self.status == 0:
@@ -36,14 +38,14 @@ class MessageTimer():
             return "SINGLE TICK"
         elif self.status == 2:
             return "DOUBLE TICK"
+        elif self.status == -1:
+            return "MESSAGE EXPIRED"
         else:
             return "STATUS UNDEFINED"
 
     '''Main Function'''
     def client(self):
-        rospy.logwarn("client ongoing")
         self.watcher = rospy.Timer(rospy.Duration(1), self.countdown)
-        rospy.logerr("SLEEPING FINISHED")
 
 class Manager():
     def __init__(self):
@@ -51,19 +53,18 @@ class Manager():
         self.messages = {}
     
     def watcher(self):
-        rospy.Subscriber("ogc/to_despatcher", LinkMessage, self.incoming_message)
+        rospy.Subscriber("ogc/to_despatcher", LinkMessage, self.sent_commands)
         # Put another subscriber in that listens to messages sent to rqt
         rospy.spin()
     
-    def incoming_message(self, data):
+    def sent_commands(self, data):
         if data.id not in self.messages:
-            rospy.loginfo("ADDING NEW ENTRY")
             self.messages[data.id] = MessageTimer(data.data, data.id)
             self.messages[data.id].client()
         else:
-            rospy.loginfo("LAME LA ALRDY RUNNING")
             self.messages[data.id].status += 1
-            self.messages[data.id].advance()
+            self.messages[data.id].stop_timer()
+            self.messages[data.id].client()
 
 if __name__=='__main__':
     run = Manager()
