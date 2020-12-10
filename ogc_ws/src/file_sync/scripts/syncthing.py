2150a0e89341174f3a3ad960c31c63fbe833ba67
Rumesh Sudhaharan <rumesh986@gmail.com>
2020-12-10 17:03:45 +0800

    separate syncthing from ROS

--- ogc_ws/src/file_sync/scripts/syncthing.py
+++ ogc_ws/src/file_sync/scripts/syncthing.py
@@ -26,26 +26,20 @@ import subprocess
 import json
 
 class Syncthing:
-	def __init__(self):
+	# def __init__(self, connected_pub, disconnected_pub, error_pub):
+	def __init__(self, error_pub):
 		self.host = "http://localhost:8384"
+		self._error_pub = error_pub
 
-		rospy.wait_for_service("identifiers/set/st_id")
-		rospy.wait_for_service("identifiers/get/id")
+		self.parse_config()
 
-		self.parse()
-
-		self._error_pub = rospy.Publisher("ogc/to_despatcher/error", String, queue_size=5)
-		self._connected_pub = rospy.Publisher("ogc/files/connected", UInt8, queue_size=5)
-		self._disconnected_pub = rospy.Publisher("ogc/files/disconnected", UInt8, queue_size=5)
 		self._connected_pub_str = rospy.Publisher("ogc/files/connected/string", String, queue_size=5)
 		self._disconnected_pub_str = rospy.Publisher("ogc/files/disconnected/string", String, queue_size=5)
 
-		self.set_device = rospy.ServiceProxy("identifiers/set/st_id", SetDetails)
-		self.get_device = rospy.ServiceProxy("identifiers/get/id", GetId)
 
-		self.set_id()
+		# self.set_id()
 
-	def parse(self):
+	def parse_config(self):
 		home_dir = str(Path.home())
 		config_path = home_dir + "/.config/syncthing/config.xml"
 		if not Path(config_path).is_file():
@@ -89,29 +83,52 @@ class Syncthing:
 	def resume(self):
 		self._post("/rest/system/resume")
 
-	def event_subscribe(self):
-		last_id = 0
-		while True:
-			rospy.loginfo("MAKING REQUEST")
-			response = self._get("/rest/events?events=DeviceConnected,DeviceDisconnected&limit=1&since="+str(last_id))
+	def get_event(self, last_id):
+		response = self._get("/rest/events?events=DeviceConnected,DeviceDisconnected&limit=1&since="+str(last_id))
+		
+		if not response or len(response) == 0:
+			return None
+		return response[0]
+
+	def get_devices(self):
+		devices = self._get("/rest/config/devices")
+
+		known_devices = []
+		for device in devices:
+			print(f'{device["name"]} {device["deviceID"]}')
+			known_devices.append(device["deviceID"])
+
+		return known_devices
+
+	# def event_subscribe(self):
+	# 	last_id = 0
+	# 	while True:
+	# 		rospy.loginfo("MAKING REQUEST")
+	# 		response = self._get("/rest/events?events=DeviceConnected,DeviceDisconnected&limit=1&since="+str(last_id))
 			
-			if response is None:
-				return
+	# 		if response is None:
+	# 			return
 
-			print(json.dumps(response, sort_keys=True, indent=4))
-			if len(response) == 0:
-				continue
+	# 		print(response)
 
-			last_id = response[0]["id"]
-			device_id = response[0]["data"]["id"]
+	# 		print(json.dumps(response, sort_keys=True, indent=4))
+	# 		if len(response) == 0:
+	# 			continue
 
-			id_request = GetIdRequest()
-			id_request.type = 5
-			id_request.data = device_id
+	# 		last_id = response[0]["id"]
+	# 		device_id = response[0]["data"]["id"]
 
-			system_id = self.get_device(id_request)
+	# 		id_request = GetIdRequest()
+	# 		id_request.type = 5
+	# 		id_request.data = device_id
 
-			if response[0]["type"] == "DeviceConnected":
-				self._connected_pub_str.publish(device_id) if system_id.id == 0 else self._connected_pub.publish(system_id.id)
-			elif response[0]["type"] == "DeviceDisconnected":
-				self._disconnected_pub_str.publish(device_id) if system_id.id == 0 else self._disconnected_pub.publish(system_id.id)
+	# 		system_id = self.get_device(id_request)
+
+	# 		if response[0]["type"] == "DeviceConnected":
+	# 			self._connected_pub.publish(system_id.id)
+	# 			# self._connected_pub_str.publish(device_id) if system_id.id == 0 else self._connected_pub.publish(system_id.id)
+	# 		elif response[0]["type"] == "DeviceDisconnected":
+	# 			self._disconnected_pub.publish(system_id.id)
+	# 			# self._disconnected_pub_str.publish(device_id) if system_id.id == 0 else self._disconnected_pub.publish(system_id.id)
+
+	
