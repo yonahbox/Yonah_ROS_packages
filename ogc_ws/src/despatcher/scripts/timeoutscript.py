9ba32ed2d0138c226e5bb66ab3a8826a0768313a
danikrisdayadi <kanisiusdani@gmail.com>
2020-12-11 11:29:13 +0800

    Changed increment wrapping algorithm

--- ogc_ws/src/despatcher/scripts/timeoutscript.py
+++ ogc_ws/src/despatcher/scripts/timeoutscript.py
@@ -22,6 +22,6 @@ def increment():
     global uuid
     uuid += 1
     if uuid > 255:
-        uuid -= 255
+        uuid = 0
     rospy.logwarn("increment is called, UUID " + str(uuid))
     return uuid
